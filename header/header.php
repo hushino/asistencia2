2e60439cb800cc0707616971c061400499a00dd0
hushino <frostshock@outlook.com>
2020-05-29 10:27:11 -0300

    f

--- header/header.php
+++ header/header.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Asistencia</a>
+  <a class="navbar-brand" href="/asistencia/inicio.php">Asistencia</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -8,7 +8,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="/asistencia/inicio.php">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/asistencia/inicio.php">Inicio <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="/asistencia/cargarasistencias.php">Cargar Asistencias</a>
